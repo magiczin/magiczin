03eece90a0a594bce9926f68123ff0c628b94dd1
Lucas Mansur <lucas.mansur2@gmail.com>
2018-01-21 19:02:20 -0200

    Group sealed cards by quantity

--- app/services/sealed/create.rb
+++ app/services/sealed/create.rb
@@ -1,14 +1,15 @@
 module Sealed
   class Create
     def self.perform(set_code)
-      [].tap do |cards|
+      Hash.new(0).tap do |cards_hash|
         6.times do
           booster = Booster.create
-
           Boosters::CreateCards.perform(set_code, booster)
-          cards << booster.cards
+          booster.cards.map do |card|
+            cards_hash[card.name] += 1
+          end 
         end
-      end.flatten
+      end
     end
   end
 end
