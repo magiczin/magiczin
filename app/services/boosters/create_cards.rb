26868384e884c829b5cbe86d90f0852fe1bc7d79
Lucas Mansur <lucas.mansur2@gmail.com>
2018-01-25 22:57:21 -0200

    Skip basic land when opening booster

--- app/services/boosters/create_cards.rb
+++ app/services/boosters/create_cards.rb
@@ -3,9 +3,14 @@ module Boosters
     def self.perform(set_code, booster)
       [].tap do |cards|
         Fetch.perform(set_code).map do |card|
+          next if basic_land?(card)
           cards << Card.create_from_mtg_card(card, booster)
         end 
       end
+    end
+
+    def self.basic_land?(card)
+      card.type.include?("Basic") && card.type.include?("Land")
     end
   end
 end
