0ee4be2d422a9af86cfb33e2d0c7fbe4293a62ef
Lucas Mansur <lucas.mansur2@gmail.com>
2018-01-25 22:37:29 -0200

    Remove booster self destruction

--- app/models/booster.rb
+++ app/models/booster.rb
@@ -2,12 +2,4 @@ class Booster < ApplicationRecord
   belongs_to :card_set, optional: true
 
   has_many :cards, dependent: :destroy
-
-  after_commit :enqueue_destruction, on: :create
-
-  private
-
-  def enqueue_destruction
-    DestroyBooster.perform_in(1.hour, id) 
-  end
 end
--- app/workers/destroy_booster.rb
+++
@@ -1,7 +0,0 @@
-class DestroyBooster
-  include Sidekiq::Worker
-  
-  def perform(booster_id)
-    Booster.destroy(booster_id)
-  end
-end
