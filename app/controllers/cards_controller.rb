bbfc3e645fcb50dcc39d486877c073b779f3285a
Lucas Mansur <lucas.mansur2@gmail.com>
2018-01-26 18:13:44 -0200

    Remove cards index

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,2 @@
 class CardsController < ApplicationController
-  def index
-    @cards = scope.cards.order(:colors, :cmc, :name)
-  end
-
-  private
-
-  def scope
-    @scope ||= params[:scope].constantize.find(params[:scope_id])
-  end
 end
