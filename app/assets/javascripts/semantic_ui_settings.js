0e33aa94f03fd865557ba4da18581b92a6506c45
Leandro Thimóteo <leandro.s.thimoteo@gmail.com>
2018-01-28 19:36:12 -0200

    Change header and sidebar layout

--- app/assets/javascripts/semantic_ui_settings.js
+++ app/assets/javascripts/semantic_ui_settings.js
@@ -1,3 +1,4 @@
 $(document).on('turbolinks:load', function(){
   $('.ui.search.dropdown').dropdown({fullTextSearch: true});
+  $('.menu .dropdown').dropdown();
 })
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def active_class(target_controller)
+    return 'active' if params[:controller] == target_controller.to_s &&
+      %w(index new show).include?(params[:action])
+  end
 end
