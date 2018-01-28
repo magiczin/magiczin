a3e13faa7fcaf7b218925277bdbaa05c570d65a3
Leandro Thimóteo <leandro.s.thimoteo@gmail.com>
2018-01-28 21:41:10 -0200

    Add behavior to accordion components and add counter to deck and junk

--- app/assets/javascripts/semantic_ui_settings.js
+++ app/assets/javascripts/semantic_ui_settings.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function(){
   $('.ui.search.dropdown').dropdown({fullTextSearch: true});
   $('.menu .dropdown').dropdown();
-  $('.dropdown').dropdown();
+  $('.ui.accordion').accordion();
 })
