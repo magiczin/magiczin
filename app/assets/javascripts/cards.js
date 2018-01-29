2c441d2d9116fcc7f186d9c1a4c133f49cbd8dfb
Lucas Mansur <lucas.mansur2@gmail.com>
2018-01-28 22:17:26 -0200

    Fix css class names

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -2,13 +2,13 @@ $(document).on('turbolinks:load', function() {
   $('.card-image').on('dblclick', function() {
     var id = $(this).parent('.card').data('id')
     var sidebarDataId = '[data-id="' + id + '"]'
-    var $sidebarCard = $('.side-deck' + sidebarDataId)
+    var $sidebarCard = $('.js-side-deck' + sidebarDataId)
 
     $checkbox = $(this).siblings('input[type=checkbox]')
     $checkbox.prop('checked', false)
     $(this).closest('.column').remove()
-    $sidebarCard.appendTo('.junk.menu')
-    $sidebarCard.removeClass('side-deck')
+    $sidebarCard.appendTo('.js-junk.menu')
+    $sidebarCard.removeClass('js-side-deck')
     $sidebarCard.addClass('junk-deck')
   })
 })
