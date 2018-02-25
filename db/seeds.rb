a297ceecbbe1111547c93d9c0386027a56c6f738
Lucas Mansur <lucas.mansur2@gmail.com>
2018-02-25 02:42:37 -0300

    Add seed to import cards and sets from json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+sets = JSON.parse(File.read("db/sets.json"))
+sets.map do |_set_code, set|
+  next if set["type"] != "expansion"
 
-sets = MTG::Set.all
-sets.map do |set|
+  p "Creating set: #{set['name']}"
   CardSet.create(
-    name: set.name, code: set.code, 
-    block: set.block, set_type: set.type,
-    release_date: set.release_date)
+    name: set["name"], code: set["code"], set_type: set["type"],
+    release_date: set["release_date"], block: set["block"]
+  )
+  p "Created set: #{set['name']}"
+  set["cards"].map do |card|
+    p "Creating card: #{card['name']}"
+    Card.create(
+      name: card["name"], mana_cost: card["manaCost"], cmc: card["cmc"],
+      colors: card["colors"], card_type: card["type"], supertypes: card["supertypes"],
+      subtypes: card["subtypes"], text: card["text"], flavor: card["flavor"],
+      power: card["power"], toughness: card["toughness"], loyalty: card["loyalty"],
+      rulings: card["rulings"], set_name: set["name"], set: set["code"]
+    )
+    p "Created card: #{card['name']}"
+  end
 end
